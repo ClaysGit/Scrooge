629e636804ba472fdb94adffd55e31f0a1be6e85
Clayton Willey <clayton@careevolution.com>
2014-07-04 11:35:46 -0400

    Add python mintcloudclient

--- SimpleTransactionExporter.Launcher/Program.cs
+++ SimpleTransactionExporter.Launcher/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SimpleTransactionExporter;
+
 namespace SimpleTransactionExporter.Launcher
 {
 	class Program
 	{
 		static void Main( string[] args )
 		{
+			var transactionDownloader = new SimpleTransactionExporter();
+
+			var csv = transactionDownloader.GetUnflaggedTransactions();
+
+			Console.Write( String.Format( "Received response {0} characters long\n", csv.Length ) );
 		}
 	}
 }
---
+++ SimpleTransactionExporter/Host.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTransactionExporter
+{
+	public class SimpleTransactionExporter
+	{
+		public SimpleTransactionExporter()
+		{
+
+		}
+
+		public void LoadCredentials()
+		{
+		
+		}
+
+		public bool LoadAllTransactions()
+		{
+			return true;
+		}
+	}
+}
