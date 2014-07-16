4772d3b9dd03015be4288deeef997850b209e07d
Clayton Willey <clayton@careevolution.com>
2014-07-15 22:56:27 -0400

    Add new website interaction library

--- SimpleTransactionExporter.Launcher/Program.cs
+++ SimpleTransactionExporter.Launcher/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using SimpleTransactionExporter;
+using SimpleWebsiteInteractionLibrary;
 
 namespace SimpleTransactionExporter.Launcher
 {
@@ -12,11 +12,71 @@ namespace SimpleTransactionExporter.Launcher
 	{
 		static void Main( string[] args )
 		{
-			var transactionDownloader = new SimpleTransactionExporter();
+			GenerateSWIFileForUMCU( "umcu.xml" );
+		}
 
-			var csv = transactionDownloader.LoadAllTransactions();
+		public static void GenerateSWIFileForUMCU( string filename )
+		{
+			var umcuSite = new SWISite()
+			{
+				InitialUrl = "https://www.umcu.org/",
+				Page1 = new SWIPage()
+				{
+					ActionUrl = "https://my.umcu.org/User/AccessSignin/Username",
+					Optional = false,
+					Fields = new List<SWIField>()
+					{
+						new SWIField()
+						{
+							RequiredLabel = null,
+							Data = "TTT_USERNAME_TTT",
+							Name = "UsernameField"
+						}
+					}
+				},
+				Page2 = new SWIPage()
+				{
+					ActionUrl = "https://my.umcu.org/User/AccessSignin/Password",
+					Optional = false,
+					Fields = new List<SWIField>()
+					{
+						new SWIField()
+						{
+							RequiredLabel = null,
+							Data = "TTT_PASSWORD_TTT",
+							Name = "PasswordField"
+						}
+					}
+				},
+				Page3 = new SWIPage()
+				{
+					ActionUrl = "https://my.umcu.org/User/AccessSignin/Challenge",
+					Optional = true,
+					Fields = new List<SWIField>()
+					{
+						new SWIField()
+						{
+							RequiredLabel = "TTT_SECQUES1_LABEL_TTT",
+							Data = "TTT_SECQUES1_TTT",
+							Name = "Answer"
+						},
+						new SWIField()
+						{
+							RequiredLabel = "TTT_SECQUES2_LABEL_TTT",
+							Data = "TTT_SECQUES2_TTT",
+							Name = "Answer"
+						},
+						new SWIField()
+						{
+							RequiredLabel = "TTT_SECQUES3_LABEL_TTT",
+							Data = "TTT_SECQUES3_TTT",
+							Name = "Answer"
+						}
+					}
+				}
+			};
 
-			Console.Write( "Received response XX characters long\n" );
+			SWISite.ToXmlFile( umcuSite, filename );
 		}
 	}
 }
---
+++ SimpleWebsiteInteractionLibrary/Site.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SimpleWebsiteInteractionLibrary
+{
+	public class SWISite
+	{
+		public static SWISite FromXmlFile( string filePath )
+		{
+			var xmlSerializer = new XmlSerializer( typeof( SWISite ) );
+			var fileStream = new FileStream( filePath, FileMode.Open );
+			return (SWISite)xmlSerializer.Deserialize( fileStream );
+		}
+
+		public static void ToXmlFile( SWISite site, string filePath )
+		{
+			var xmlSerializer = new XmlSerializer( typeof( SWISite ) );
+			var fileStream = new FileStream( filePath, FileMode.CreateNew );
+			xmlSerializer.Serialize( fileStream, site );
+		}
+
+		public string InitialUrl { get; set; }
+		public SWIPage Page1 { get; set; }
+		public SWIPage Page2 { get; set; }
+		public SWIPage Page3 { get; set; }
+	}
+
+	public class SWIPage
+	{
+		public SWIPage()
+		{
+			Fields = new List<SWIField>();
+		}
+
+		public string ActionUrl { get; set; }
+		public bool Optional { get; set; }
+
+		public List<SWIField> Fields { get; set; }
+	}
+
+	public class SWIField
+	{
+		public string RequiredLabel { get; set; }
+		public string Data { get; set; }
+		public string Name { get; set; }
+	}
+}
